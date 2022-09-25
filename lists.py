53e72ef9ba374aacdce02c834e58b14333318640
Yash Goyal <yash.goyal.classic@gmail.com>
2022-09-25 13:09:51 +0530

    final commit

---
+++ lists.py
@@ -0,0 +1,85 @@
+colors = [0xEADDCA, 0xFFBF00, 	0xFBCEB1, 	0xF5F5DC, 	0xE1C16E, 	0xFFEA00, 	0xFDDA0D, 	0xFFFF8F, 0xDFFF00, 	0xE4D00A, 	0xFFF8DC, 	0xFFFDD0, 	0x8B8000, 	0xFAD5A5, 	0xC2B280, 	0xEEDC82, 	0xE49B0F,
+          0xFFD700, 	0xFFC000, 0xDAA520, 	 0xFCF55F, 	0xFFFFF0, 	0xF8DE7E, 	0xF0E68C, 	0xFAFA33, 	0xFBEC5D, 0xF4BB44, 	0xFFDB58,	0xFADA5E, 	0xFFDEAD, 0xECFFDC, 	0xFFFAA0,
+          0xFFE5B4, 0xC9CC3F, 	0xB4C424, 	0x93C572, 	0xF4C430, 0xF3E5AB, 	0xC4B454, 0xF5DEB3, 	0xFFFF00, 	0xFFAA33, 0x00FFFF, 	0xF0FFFF, 	0x89CFF0, 	0x0000FF, 	0x7393B3, 	0x088F8F,
+          0x0096FF, 	0x5F9EA0, 	0x0047AB, 	0x6495ED, 	0x00FFFF, 	0x00008B, 	0x6F8FAF, 	0x1434A4, 	0x7DF9FF, 	0x6082B6,
+          0x00A36C, 	0x3F00FF, 	0xA7C7E7, 	0xB6D0E2, 	0x9FE2BF, 	0x008080, 	0x40E0D0, 	0x00FFFF, 	0x7FFFD4, 	0x454B1B, 	0x088F8F, 	0x00FFFF,	0x7DF9FF, 	0x228B22, 	0x90EE90, 	0x32CD32,
+          0x0FFF50, 	0x93C572,	0x8A9A5B, 	0x2E8B57, 	0x9FE2BF, 	0xFFAC1C, 	0xCD7F32, 	0xB87333, 	0xFF7F50, 0xF88379, 	0xFFD580, 	0xC04000, 	0xFF5F1F, 	0xFAC898,	0xFFE5B4, 	0xEC5800, 0xF89880, 	0xDC143C, 	0xAA336A, 	0xFF69B4, 	0xFFB6C1, 	0xF3CFC6, 	0xF8C8DC, 	0xFAA0A0, 	0xFFC0CB, 	0xE37383]
+
+riddles = [
+    "**Riddle:** What has to be broken before you can use it.\n**Answer:** ||An egg|| ",
+    "**Riddle:** I’m tall when I’m young, and I’m short when I’m old. What am I?\n**Answer:** ||A Candle||",
+    "**Riddle:** What month of the year has 28 days?\n**Answer:** ||All of them||",
+    "**Riddle:** What is full of holes but still holds water?\n**Answer:** ||A Sponge||",
+    "**Riddle:** What question can you never answer yes to?\n**Answer:** ||Are you asleep yet?||",
+    "**Riddle:** What is always in front of you but can’t be seen?\n**Answer:** ||The future||",
+    "**Riddle:** There’s a one-story house in which everything is yellow. Yellow walls, yellow doors, yellow furniture. What color are the stairs?\n**Answer:** ||here aren't any—it’s a one-story house||",
+    "**Riddle:** What can you break, even if you never pick it up or touch it?\n**Answer:** ||A promise||",
+    "**Riddle:** What goes up but never comes down?\n**Answer:** ||Your age||",
+    "**Riddle:** A man who was outside in the rain without an umbrella or hat didn’t get a single hair on his head wet. Why?\n**Answer:** ||He was bald||",
+    "**Riddle:** I follow you all the time and copy your every move, but you can’t touch me or catch me. What am I?\n**Answer:** ||Your shadow||",
+    "**Riddle:** What has many keys but can’t open a single lock?\n**Answer:** ||A piano||",
+    "**Riddle:** What can you hold in your left hand but not in your right?\n**Answer:** ||Your right elbow||",
+    "**Riddle:** What is black when it’s clean and white when it’s dirty?\n**Answer:** ||A chalkboard||",
+    "**Riddle:** What gets bigger when more is taken away?\n**Answer:** ||A hole||",
+    "**Riddle:** I’m light as a feather, yet the strongest person can’t hold me for five minutes. What am I?\n**Answer:** ||Your breath||",
+    "**Riddle:** I speak without a mouth and hear without ears. I have no body, but come alive with wind. What am I?\n**Answer:** ||Echo|| ",
+    "**Riddle:** What begins with an “e” and only contains one letter?\n**Answer:** ||An Envelope||"
+    "**Riddle:** A man goes to the store and buys one dozen eggs. As she is going home, all but three break. How many eggs are left unbroken? \n**Answer:** ||Three||",
+    "**Riddle:** A word I know, six letters it contains, remove one letter and 12 remains. What is it? \n**Answer:** ||Dozens||"
+]
+
+ball = ["definitely", "no", "maybe", "obviously", "obviously not", "I don't know"]
+
+ball_arg = ["owner", "molecule", "you suck", "u suck", "Aurora", "aurora"]
+
+rp =["rock", "scissors", "paper"]
+
+roasts = [
+    "you watch tiktok",
+    "you play forknite on a phone",
+    "yo think about a helicopt- oops you cant think can you",
+    "you want a nasty look? go find a mirror",
+    'Where’s your off button?',
+    'I’m not shy. I just don’t like you.',
+    'You have more faces than Mount Rushmore.',
+    'I’m jealous of people who don’t know you.',
+    'You’re entitled to your incorrect opinion.',
+    'I’m visualizing duck tape over your mouth.',
+    'You’re the reason I prefer animals to people.',
+    'If I had a face like yours, I’d sue my parents.',
+    'I’d smack you, but that would be animal abuse.',
+    'Hey, I found your nose, it’s in my business again!',
+    'I might be crazy, but crazy is better than stupid.',
+    'Is there an app I can download to make you disappear',
+    'The people who know me the least have the most to say.',
+    'I am allergic to stupidity, so I break out in sarcasm.',
+    'I didn’t change. I grew up. You should try it sometime.',
+    "You’re a grey sprinkle on a rainbow cupcake.",
+    "If your brain was dynamite, there wouldn’t be enough to blow your hat off.",
+    "You are more disappointing than an unsalted pretzel.",
+    "Light travels faster than sound, which is why you seemed bright until you spoke.",
+    "You have so many gaps in your teeth it looks like your tongue is in jail.",
+    "I’ll never forget the first time we met. But I’ll keep trying.",
+    "Hold still. I’m trying to imagine you with a personality.",
+    "If you’re going to be two-faced, at least make one of them pretty.",
+    "You are like a cloud. When you disappear, it’s a beautiful day.",
+    "I love what you’ve done with your hair. How do you get it to come out of your nostrils like that?",
+    "Child, I’ve forgotten more than you ever knew.",
+    ]
+
+vb = "<a:9358vibedance:827049139463258153>"
+ww = "<a:7250umwtf:827049137580015617>"
+xp = "<:auroraxp:835042919793688647>"
+coin = "<:a_coin:845896591641411625>"
+dpe = "<:durabilitypotion:835761213307748383>"
+hpe = "<:healthpotion:835760441732235284>"
+vpe = "<:vigourpotion:835760905262202880>"
+dash = "<:auroradash:835765195216257054>"
+qge = "<:qgun:844452209029677066>"
+ebe = "<:ebazooka:844452208842113035>"
+ame = "<:aurora_molecule:844217688293507073>"
+woe = "<:witchs_orb:844221988419534869>"
+ate = "<:a_attack:845895449918570516>"
+dfe = "<:a_shield:845895450065502238>"
+hae = "<:a_heart:845895449910312990>"
+mbe = "<:a_mysterybox:849172705437417482>"
